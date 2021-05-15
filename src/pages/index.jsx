0b0779aa3d01220618b01f54109606df290c9fb1
mrmtsu <81202muramatsu@gmail.com>
2021-05-15 11:39:42 +0900

    useEffectの確認

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,4 @@
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
 import { Footer } from '../components/Footer'
 import { Header } from '../components/Header'
 import { Main } from '../components/Main'
@@ -11,6 +11,17 @@ export default function Home() {
     console.log(e.target.href);
     e.preventDefault();
     alert(foo);
+  }, []);
+
+  useEffect(() => {
+    console.log('マウント時');
+    document.body.style.backgroundColor = "lightblue";
+
+    // unmount時の処理
+    return () => {
+      console.log('アンマウント時');
+      document.body.style.backgroundColor = "";
+    }
   }, []);
 
   return (
