0f1b3f1f9abef9920251f5efac86821a91b6debc
mrmtsu <81202muramatsu@gmail.com>
2021-05-15 13:44:20 +0900

    破壊的・非破壊的メソッドの確認

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,6 +8,7 @@ export default function Home() {
   const [count, setCount] = useState(1);
   const [text, setText] = useState("");
   const [isShow, setIsShow] = useState(true);
+  const [array, setArray] = useState([]);
 
   const handleClick = useCallback(() => {
     if (count < 10) {
@@ -16,7 +17,7 @@ export default function Home() {
   }, [count]);
 
   const handleIsShow = () => {
-    setIsShow((isShow) => !isShow);
+    setIsShow((prevIsShow) => !prevIsShow);
   };
 
   const onChangeText = useCallback((e) => {
@@ -26,6 +27,18 @@ export default function Home() {
     }
     setText(e.target.value);
   }, []);
+
+  const handleAdd = useCallback(() => {
+    setArray((prevArray) => {
+      if (prevArray.some(item => item === text)) {
+        alert('同じ要素がすでに存在します');
+        setText("");
+        return prevArray;
+      }
+      setText("");
+      return [...prevArray, text];
+    });
+  }, [text]);
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
@@ -43,6 +56,14 @@ export default function Home() {
       <button onClick={handleClick}>ボタン</button>
       <button onClick={handleIsShow}>{ isShow ? "非表示" : "表示"}</button>
       <input type="text" value={text} onChange={onChangeText} />
+      <button onClick={handleAdd}>追加</button>
+      <ul>
+        {array.map((item) => {
+          return (
+            <li key={item}>{item}</li>
+          )
+        })}
+      </ul>
       <Main page="index" />
       <Footer />
     </div>
