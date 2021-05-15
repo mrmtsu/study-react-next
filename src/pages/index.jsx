f7fc3c8d3933d2463877e1f0228b943b7aac3acc
mrmtsu <81202muramatsu@gmail.com>
2021-05-15 11:15:39 +0900

    クリックイベントの確認

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,24 @@
-import Head from 'next/head'
+import { useCallback } from 'react'
 import { Footer } from '../components/Footer'
 import { Header } from '../components/Header'
 import { Main } from '../components/Main'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const foo = 1
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+
   return (
     <div className={styles.container}>
       <Header />
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
       <Main page="index" />
       <Footer />
     </div>
